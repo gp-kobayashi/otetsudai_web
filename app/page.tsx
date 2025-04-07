820c2546a23d1c08561118bf9d72a9f294c4bf31
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-04-07 14:56:03 +0900

    [add]文章の追加

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div>
       <div className={styles.main_container}>
         <h2>お手伝いをしましょう</h2>
+        <p>イラスト、音楽、動画編集などのお手伝いをつなぐサイトです</p>
       </div>
     </div>
   );
