bb61f7db4477f27b0c02f3c72bb0fa1e9eb526cc
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-04-07 17:12:07 +0900

    [add]mainPageにListコンポーネントの追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import styles from "./page.module.css";
-
+import List from "./components/recruitment/mainPage/List";
 export default function Home() {
   return (
     <div>
       <div className={styles.main_container}>
         <h2>お手伝いをしましょう</h2>
         <p>イラスト、音楽、動画編集などのお手伝いをつなぐサイトです</p>
+      </div>
+      <div>
+        <List />
       </div>
     </div>
   );
