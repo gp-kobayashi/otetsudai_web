9ee8921df34102814af024f382ce75815e9da3e3
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-04-07 16:07:52 +0900

    [clean]prettierの自動整形

--- database.types.ts
+++ database.types.ts
@@ -4,135 +4,135 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
   public: {
     Tables: {
       comments: {
         Row: {
-          content: string
-          created_at: string | null
-          id: number
-          recruitment_id: number
-          updated_at: string | null
-          user_id: string
-        }
+          content: string;
+          created_at: string | null;
+          id: number;
+          recruitment_id: number;
+          updated_at: string | null;
+          user_id: string;
+        };
         Insert: {
-          content: string
-          created_at?: string | null
-          id?: number
-          recruitment_id: number
-          updated_at?: string | null
-          user_id: string
-        }
+          content: string;
+          created_at?: string | null;
+          id?: number;
+          recruitment_id: number;
+          updated_at?: string | null;
+          user_id: string;
+        };
         Update: {
-          content?: string
-          created_at?: string | null
-          id?: number
-          recruitment_id?: number
-          updated_at?: string | null
-          user_id?: string
-        }
+          content?: string;
+          created_at?: string | null;
+          id?: number;
+          recruitment_id?: number;
+          updated_at?: string | null;
+          user_id?: string;
+        };
         Relationships: [
           {
-            foreignKeyName: "comments_recruitment_id_fkey"
-            columns: ["recruitment_id"]
-            isOneToOne: false
-            referencedRelation: "recruitments"
-            referencedColumns: ["id"]
+            foreignKeyName: "comments_recruitment_id_fkey";
+            columns: ["recruitment_id"];
+            isOneToOne: false;
+            referencedRelation: "recruitments";
+            referencedColumns: ["id"];
           },
           {
-            foreignKeyName: "comments_user_id_fkey"
-            columns: ["user_id"]
-            isOneToOne: false
-            referencedRelation: "profiles"
-            referencedColumns: ["id"]
+            foreignKeyName: "comments_user_id_fkey";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "profiles";
+            referencedColumns: ["id"];
           },
-        ]
-      }
+        ];
+      };
       profiles: {
         Row: {
-          avatar_url: string | null
-          full_name: string | null
-          id: string
-          updated_at: string | null
-          username: string | null
-          website: string | null
-        }
+          avatar_url: string | null;
+          full_name: string | null;
+          id: string;
+          updated_at: string | null;
+          username: string | null;
+          website: string | null;
+        };
         Insert: {
-          avatar_url?: string | null
-          full_name?: string | null
-          id: string
-          updated_at?: string | null
-          username?: string | null
-          website?: string | null
-        }
+          avatar_url?: string | null;
+          full_name?: string | null;
+          id: string;
+          updated_at?: string | null;
+          username?: string | null;
+          website?: string | null;
+        };
         Update: {
-          avatar_url?: string | null
-          full_name?: string | null
-          id?: string
-          updated_at?: string | null
-          username?: string | null
-          website?: string | null
-        }
-        Relationships: []
-      }
+          avatar_url?: string | null;
+          full_name?: string | null;
+          id?: string;
+          updated_at?: string | null;
+          username?: string | null;
+          website?: string | null;
+        };
+        Relationships: [];
+      };
       recruitments: {
         Row: {
-          created_at: string | null
-          explanation: string
-          id: number
-          tag: Database["public"]["Enums"]["tag_enum"]
-          title: string
-          updated_at: string | null
-          user_id: string
-        }
+          created_at: string | null;
+          explanation: string;
+          id: number;
+          tag: Database["public"]["Enums"]["tag_enum"];
+          title: string;
+          updated_at: string | null;
+          user_id: string;
+        };
         Insert: {
-          created_at?: string | null
-          explanation: string
-          id?: number
-          tag: Database["public"]["Enums"]["tag_enum"]
-          title: string
-          updated_at?: string | null
-          user_id: string
-        }
+          created_at?: string | null;
+          explanation: string;
+          id?: number;
+          tag: Database["public"]["Enums"]["tag_enum"];
+          title: string;
+          updated_at?: string | null;
+          user_id: string;
+        };
         Update: {
-          created_at?: string | null
-          explanation?: string
-          id?: number
-          tag?: Database["public"]["Enums"]["tag_enum"]
-          title?: string
-          updated_at?: string | null
-          user_id?: string
-        }
+          created_at?: string | null;
+          explanation?: string;
+          id?: number;
+          tag?: Database["public"]["Enums"]["tag_enum"];
+          title?: string;
+          updated_at?: string | null;
+          user_id?: string;
+        };
         Relationships: [
           {
-            foreignKeyName: "recruitments_user_id_fkey"
-            columns: ["user_id"]
-            isOneToOne: false
-            referencedRelation: "profiles"
-            referencedColumns: ["id"]
+            foreignKeyName: "recruitments_user_id_fkey";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "profiles";
+            referencedColumns: ["id"];
           },
-        ]
-      }
-    }
+        ];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      tag_enum: "Video" | "Text" | "Audio" | "programming" | "design" | "other"
-    }
+      tag_enum: "Video" | "Text" | "Audio" | "programming" | "design" | "other";
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -145,7 +145,7 @@ export type Tables<
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
@@ -153,11 +153,11 @@ export type Tables<
         PublicSchema["Views"])
     ? (PublicSchema["Tables"] &
         PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
+        Row: infer R;
       }
       ? R
       : never
-    : never
+    : never;
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -168,17 +168,17 @@ export type TablesInsert<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
+        Insert: infer I;
       }
       ? I
       : never
-    : never
+    : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -189,17 +189,17 @@ export type TablesUpdate<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
+        Update: infer U;
       }
       ? U
       : never
-    : never
+    : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -212,14 +212,14 @@ export type Enums<
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+    : never;
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof PublicSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
@@ -227,4 +227,4 @@ export type CompositeTypes<
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
     ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+    : never;
