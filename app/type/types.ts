9fbc76fc10bfc1758aadd34e0ea0425ee0a15cdc
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-04-08 15:26:07 +0900

    [add]型定義ProfileとRecruitmentWithProfileの追加

--- app/type/types.ts
+++ app/type/types.ts
@@ -4,6 +4,11 @@ export type Recruitment = Database["public"]["Tables"]["recruitments"]["Row"];
 
 export type Comment = Database["public"]["Tables"]["comments"]["Row"];
 
+export type Profile = Database["public"]["Tables"]["profiles"]["Row"];
+
+export type RecruitmentWithProfile = Database["public"]["Tables"]["recruitments"]["Row"] & {
+  avatar_url: string } &{username: string | null}; 
+
 export type SupabaseResponse<T> = {
   data: T | null;
   error: Error | null;
