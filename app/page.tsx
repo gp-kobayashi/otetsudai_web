5643860db464a01252ec146c0f19a6dea72e40bd
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-04-15 16:57:56 +0900

    [add]カテゴリーページへのリンクを作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import styles from "./page.module.css";
 import List from "./components/mainPage/list";
 export default function Home() {
+  const tags = ["Video", "Text", "Audio", "programming", "design", "other"];
   return (
     <div>
       <div className={styles.main_container}>
         <h2>お手伝いをしましょう</h2>
         <p>イラスト、音楽、動画編集などのお手伝いをつなぐサイトです</p>
       </div>
+      <div className={styles.tag_container}>
+        {tags.map((tag) => (
+          <div key={tag}>
+            <a href={`/category/${tag}`} className={styles.tag}>
+              {tag}
+            </a>
+          </div>
+        ))}
+      </div>
+
       <div>
         <List />
       </div>
