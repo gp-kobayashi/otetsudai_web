323f1171fc1158d150876be3d914fc6156dbc3bc
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-04-21 15:06:27 +0900

    [add]usernameが表示されるページでuserProfileページへリンクされるよう記述

--- app/components/mainPage/list.tsx
+++ app/components/mainPage/list.tsx
@@ -6,6 +6,7 @@ import styles from "./list.module.css";
 import Image from "next/image";
 import { CiClock2 } from "react-icons/ci";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const MainPageList = () => {
   const router = useRouter();
@@ -52,7 +53,9 @@ const MainPageList = () => {
                 height={20}
                 className={styles.avatar_img}
               />
-              <p className={styles.info_item}>{recruitment.username}</p>
+              <Link href={`/userProfile/${recruitment.username}`}>
+                <p className={styles.info_item}>{recruitment.username}</p>
+              </Link>
               <CiClock2 className={styles.info_icon} />
               <p className={styles.info_item}>{recruitment.created_at}</p>
             </div>
