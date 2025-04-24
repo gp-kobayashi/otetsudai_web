2e4ee8c1a39a92bb5983f5d6006a1caecb7c50c8
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-04-24 15:07:43 +0900

    [add]ユーザー本人が募集を削除できるようにボタンの追加

--- app/userProfile/[username]/list.tsx
+++ app/userProfile/[username]/list.tsx
@@ -3,11 +3,15 @@
 import type { Recruitment } from "@/app/type/types";
 import styles from "./userProfile.module.css";
 import { useRouter } from "next/navigation";
+import { deleteRecruitment } from "@/app/supabase_function/recruitment";
+
 type props = {
   recruitmentList: Recruitment[] | null;
+  checkUsername: boolean;
 };
 
-const UserRecruitmentList = ({ recruitmentList }: props) => {
+const UserRecruitmentList = (props: props) => {
+  const { recruitmentList, checkUsername } = props;
   const router = useRouter();
   if (!recruitmentList) {
     return <div>募集がありません</div>;
@@ -15,6 +19,15 @@ const UserRecruitmentList = ({ recruitmentList }: props) => {
   const handleClick = (id: number) => {
     router.push(`/recruitment/${id}`);
   };
+
+  const deleteBtn = async (id: number) => {
+    if (!confirm("本当に削除しますか？")) {
+      return;
+    }
+    await deleteRecruitment(id);
+    router.refresh();
+  };
+
   return (
     <div>
       <h2 className={styles.recruitment_title}>募集一覧</h2>
@@ -27,7 +40,20 @@ const UserRecruitmentList = ({ recruitmentList }: props) => {
           >
             <div className={styles.list_title}>{recruitment.title}</div>
             <div>{recruitment.explanation}</div>
-            <div className={styles.list_tag}>{recruitment.tag}</div>
+            <div className={styles.list_item}>
+              <div className={styles.list_tag}>{recruitment.tag}</div>
+              {checkUsername && (
+                <button
+                  className={styles.delete_button}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deleteBtn(recruitment.id);
+                  }}
+                >
+                  削除
+                </button>
+              )}
+            </div>
           </li>
         ))}
       </ul>
