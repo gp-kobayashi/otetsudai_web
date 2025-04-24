f8b64948464c6a61eae87f5737ebb97e0d117c0e
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-04-24 13:06:18 +0900

    [add]accountページに移動した際、ログインやユーザーネームの登録がない場合、各ページに遷移

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,5 +1,7 @@
+import { redirect } from "next/navigation";
 import AccountForm from "./account-form";
 import { createClient } from "@/utils/supabase/server";
+import { fetchProfile } from "@/app/supabase_function/profile";
 
 export default async function Account() {
   const supabase = await createClient();
@@ -7,6 +9,15 @@ export default async function Account() {
   const {
     data: { user },
   } = await supabase.auth.getUser();
+
+  const { data: profile } = await fetchProfile(user?.id || "");
+  const username = profile?.username;
+  if (!user) {
+    return redirect("/login");
+  }
+  if (user && !username) {
+    return redirect("/insertUserName");
+  }
 
   return <AccountForm user={user} />;
 }
