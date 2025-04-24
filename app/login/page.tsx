b9e67b40d29ec9eb33013b1fe6673d85483cfc27
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-04-24 16:38:01 +0900

    [add]loginページのデザイン整理

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,38 @@
 import { login, signup } from "./actions";
+import styles from "./login.module.css";
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className={styles.login_container}>
+      <h2>Login or signUp</h2>
+      <form>
+        <label className={styles.login_label} htmlFor="email">
+          Email:
+        </label>
+        <input
+          className={styles.login_input}
+          id="email"
+          name="email"
+          type="email"
+          required
+        />
+        <label className={styles.login_label} htmlFor="password">
+          Password:
+        </label>
+        <input
+          className={styles.login_input}
+          id="password"
+          name="password"
+          type="password"
+          required
+        />
+        <button className={styles.btn} formAction={login}>
+          Log in
+        </button>
+        <button className={styles.btn} formAction={signup}>
+          Sign up
+        </button>
+      </form>
+    </div>
   );
 }
