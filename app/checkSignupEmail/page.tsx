f8e923984cec9a46795b40acbc66b67b36da6042
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-04-28 13:39:30 +0900

    [add]signup時にメール確認を通知するページの作成

---
+++ app/checkSignupEmail/page.tsx
@@ -0,0 +1,14 @@
+import styles from "./checkSingup.module.css";
+
+const CheckSignupEmail = () => {
+  return (
+    <div className={styles.check_email_container}>
+      <h2>サインアップ確認用のメールを送信しました</h2>
+      <p className={styles.text}>
+        メールを確認し記載されたURLから認証を完了してください。
+      </p>
+    </div>
+  );
+};
+
+export default CheckSignupEmail;
