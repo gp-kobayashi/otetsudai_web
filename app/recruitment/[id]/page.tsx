1c003c823327f766cefb5ad27d4cb41f18bacff7
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-05-02 16:16:19 +0900

    [fix]recruitmentコンポーネントの切り出し

--- app/recruitment/[id]/page.tsx
+++ app/recruitment/[id]/page.tsx
@@ -1,14 +1,10 @@
 import { getRecruitmentById } from "@/app/supabase_function/recruitment";
 import { fetchProfile } from "@/app/supabase_function/profile";
 import styles from "./recruitment.module.css";
-import Image from "next/image";
-import { CiClock2 } from "react-icons/ci";
+import RecruitmentComponent from "./recruitment";
 import CommentApp from "@/app/components/comment/app";
 import { createClient } from "@/utils/supabase/server";
-import Link from "next/link";
-import DeleteButton from "./deleteButton";
 import { redirect } from "next/navigation";
-import EditButton from "./editButton";
 
 interface Params {
   id: number;
@@ -38,27 +34,7 @@ const recruitment = async ({ params }: { params: Params }) => {
   }
   return (
     <div>
-      <div className={styles.recruitment_container}>
-        {userId === data.user_id && <EditButton id={id} />}
-        <h3 className={styles.title}>{data.title}</h3>
-        <p className={styles.text}>{data.explanation}</p>
-        <div className={styles.recruitment_user}>
-          <p className={styles.tag}>{data.tag}</p>
-          <Image
-            src={data.avatar_url}
-            className={styles.item}
-            alt="avatar"
-            width={40}
-            height={40}
-          />
-          <Link href={`/userProfile/${data.username}`}>
-            <p className={styles.item}>{data.username}</p>
-          </Link>
-          <CiClock2 className={styles.info_icon} />
-          <p className={styles.item}>{data.created_at}</p>
-          {userId === data.user_id && <DeleteButton id={id} />}
-        </div>
-      </div>
+      <RecruitmentComponent id={id} userId={userId} recruitmentData={data} />
 
       <CommentApp id={id} userId={userId} username={username} />
     </div>
