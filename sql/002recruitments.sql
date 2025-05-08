876ff584c0a745c27c782cb8233eaacd5077bdaa
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-05-08 13:05:06 +0900

    [fix]uptdated_atのタイプミス修正

--- sql/002recruitments.sql
+++ sql/002recruitments.sql
@@ -25,7 +25,7 @@ CREATE TABLE comments(
 CREATE OR REPLACE FUNCTION update_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
-    NEW.update_at = NOW();
+    NEW.updated_at = NOW();
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
