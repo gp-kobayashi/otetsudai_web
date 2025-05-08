fe2f5d7db7b458729fd63780a37e175e646384db
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-05-08 15:32:03 +0900

    [add]usernameを管理するSchemaの追加

--- app/utils/zod.ts
+++ app/utils/zod.ts
@@ -3,7 +3,7 @@ import {z} from "zod";
 export const profileSchema = z.object({
     username: z
       .string()
-      .min(1, "ユーザー名は必須です")
+      .min(3, "ユーザー名は3文字以上で入力してください")
       .max(20, "ユーザー名は20文字以内で入力してください")
       .regex(/^[a-zA-Z0-9]+$/, "ユーザー名は英数字のみ使用できます"),
     website: z
@@ -13,4 +13,12 @@ export const profileSchema = z.object({
       .or(z.literal("")), 
     avatar_url: z.string().nullable().or(z.literal("")),
     bio: z.string().nullable().or(z.literal(""))
+  });
+
+   export const usernameSchema = z.object({
+    username: z
+      .string()
+      .min(3, "ユーザー名は3文字以上で入力してください")
+      .max(20, "ユーザー名は20文字以内で入力してください")
+      .regex(/^[a-zA-Z0-9]+$/, "ユーザー名は英数字のみ使用できます"),
   });
