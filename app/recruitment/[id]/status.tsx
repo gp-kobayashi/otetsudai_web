7dfd0d3ebbdecaa6761d6aea11bb34bad1d58021
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-05-09 17:03:43 +0900

    [add]ステータス変更のselectの追加

---
+++ app/recruitment/[id]/status.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useState } from "react";
+import styles from "./recruitment.module.css";
+import { updateStatus } from "@/app/supabase_function/recruitment";
+type StatusType = "募集中" | "対応中" | "完了" | "キャンセル" | "期限切れ";
+type Props = {
+  id: number;
+  onStatusChange: (status: StatusType) => void;
+};
+
+const RecruitmentStatus = (props: Props) => {
+  const { id, onStatusChange } = props;
+  const statuses = ["募集中", "対応中", "完了", "キャンセル", "期限切れ"];
+  const [status, setStatus] = useState(statuses[0]);
+  const handleChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newStatus = e.target.value as StatusType;
+    setStatus(newStatus);
+    await updateStatus(id, newStatus);
+    onStatusChange(newStatus);
+  };
+  return (
+    <div>
+      <label className={styles.status_label}>ステータス変更</label>
+      <select value={status} onChange={handleChange}>
+        {statuses.map((statusOption) => (
+          <option key={statusOption} value={statusOption}>
+            {statusOption}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default RecruitmentStatus;
