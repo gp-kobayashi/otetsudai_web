b09ebac42097e5d4ba6aa0b248f10d9ce0b821dd
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-05-09 15:33:10 +0900

    [fix]usernameSchemaのprofileSchemaでの再利用

--- utils/zod.ts
+++ utils/zod.ts
@@ -1,11 +1,16 @@
 import {z} from "zod";
 
-export const profileSchema = z.object({
+
+export const usernameSchema = z.object({
     username: z
       .string()
       .min(3, "ユーザー名は3文字以上で入力してください")
       .max(20, "ユーザー名は20文字以内で入力してください")
       .regex(/^[a-zA-Z0-9]+$/, "ユーザー名は英数字のみ使用できます"),
+  });
+
+export const profileSchema = z.object({
+    username: usernameSchema,
     website: z
       .string()
       .url("正しいURL形式で入力してください")
@@ -15,10 +20,4 @@ export const profileSchema = z.object({
     bio: z.string().nullable().or(z.literal(""))
   });
 
-   export const usernameSchema = z.object({
-    username: z
-      .string()
-      .min(3, "ユーザー名は3文字以上で入力してください")
-      .max(20, "ユーザー名は20文字以内で入力してください")
-      .regex(/^[a-zA-Z0-9]+$/, "ユーザー名は英数字のみ使用できます"),
-  });
+   
