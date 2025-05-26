20921393883eebd142ebd54dd910e0986067201c
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-05-26 13:44:05 +0900

    [add]vitestがsupabaseの.env.localを読み込めるようにする

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dotenv from 'dotenv'
 
+dotenv.config({ path: '.env.local' }) 
 
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
