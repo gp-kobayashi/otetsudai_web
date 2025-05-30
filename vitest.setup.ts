e4f2423bd9db617eac3996ccbf40e7293b7cf7e5
gp-n-suzuki <nobuaki_s@gp1000.jp>
2025-05-30 13:51:51 +0900

    [fix] Next.jsのuseRouterをモックし、MSWでのリクエストハンドリングを強化

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,18 @@
 import "@testing-library/jest-dom/vitest";
-import { afterAll, afterEach, beforeAll } from "vitest";
+import { afterAll, afterEach, beforeAll, vi } from "vitest";
 import { APIserver } from "./test/server";
+
+// Next.jsのuseRouterをモック
+vi.mock("next/navigation", () => ({
+    useRouter: () => ({
+      push: vi.fn(),
+      back: vi.fn(),
+      forward: vi.fn(),
+      refresh: vi.fn(),
+      replace: vi.fn(),
+      prefetch: vi.fn(),
+    }),
+  }));
 
 beforeAll(() => APIserver.listen());
 afterAll(() => APIserver.close());
