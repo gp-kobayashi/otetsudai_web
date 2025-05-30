fc9a5eeae9711326b82d579b1c130af534d3e629
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-05-30 16:58:21 +0900

    [add]Paginationコンポーネントのテスト

---
+++ test/component/recruitment/pagination.test.tsx
@@ -0,0 +1,55 @@
+import { describe, expect, test, vi } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
+import Pagination from "@/components/recruitment/category/pagination";
+
+describe("Pagination", () => {
+  test("初期レンダリングが行われる", () => {
+    render(
+      <Pagination
+        currentPage={1}
+        hasNextPage={true}
+        tag={"Video"}
+        filterQuery={""}
+      />,
+    );
+    expect(screen.getByText("1")).toBeInTheDocument();
+    expect(screen.getByTestId("icon-next")).toBeInTheDocument();
+  });
+  test("前後のページリンクが表示される", () => {
+    render(
+      <Pagination
+        currentPage={2}
+        hasNextPage={true}
+        tag={"Video"}
+        filterQuery={""}
+      />,
+    );
+    expect(screen.getByTestId("icon-next")).toBeInTheDocument();
+    expect(screen.getByTestId("icon-previous")).toBeInTheDocument();
+  });
+  test("前後のページに遷移する", async () => {
+    render(
+      <Pagination
+        currentPage={2}
+        hasNextPage={true}
+        tag={"Video"}
+        filterQuery={""}
+      />,
+    );
+    const nextLink = screen.getByTestId("icon-next").closest("a");
+    const previousLink = screen.getByTestId("icon-previous").closest("a");
+    expect(nextLink).toHaveAttribute("href", "/category/Video/3");
+    expect(previousLink).toHaveAttribute("href", "/category/Video/1");
+  });
+  test("次のページがない場合、次のリンクが表示されない", () => {
+    render(
+      <Pagination
+        currentPage={3}
+        hasNextPage={false}
+        tag={"Video"}
+        filterQuery={""}
+      />,
+    );
+    expect(screen.queryByTestId("icon-next")).not.toBeInTheDocument();
+  });
+});
