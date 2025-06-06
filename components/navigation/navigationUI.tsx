5931ffac537f188b630132057b08d33766412ba9
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-06-06 14:01:54 +0900

    [clean]navigationコンポーネントをUIとサーバーコンポーネントに分離

---
+++ components/navigation/navigationUI.tsx
@@ -0,0 +1,42 @@
+import styles from "./navigation.module.css";
+import Link from "next/link";
+import { User } from "@supabase/supabase-js";
+
+type Props = {
+  user: User | null;
+  username?: string | null;
+};
+
+const NavigationUI = (props: Props) => {
+  const { user, username } = props;
+
+  return (
+    <div className={styles.navigation_container}>
+      <Link href="/" className={styles.logo}>
+        <h1>otetsudai</h1>
+      </Link>
+      <div className={styles.navigation_link}>
+        <Link href="/createRecruitment" className={styles.recruitment_link}>
+          募集する
+        </Link>
+        <Link href="/help " className={styles.help_link}>
+          help
+        </Link>
+        {user ? (
+          <Link
+            href={`/userProfile/${username}`}
+            className={styles.account_link}
+          >
+            profile
+          </Link>
+        ) : (
+          <Link href="/login" className={styles.login_link}>
+            ログイン
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default NavigationUI;
