892f1f878e393f109c03bfd68a863afb9185bebf
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-06-06 14:01:54 +0900

    [clean]navigationコンポーネントをUIとサーバーコンポーネントに分離

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
-import styles from "./navigation.module.css";
-import Link from "next/link";
+
 import { fetchProfile } from "@/lib/supabase_function/profile";
+import NavigationUI from "./navigationUI";
 
 const Navigation = async () => {
   const supabase = await createClient();
@@ -17,33 +17,7 @@ const Navigation = async () => {
     username = data?.username || null;
   }
 
-  return (
-    <div className={styles.navigation_container}>
-      <Link href="/" className={styles.logo}>
-        <h1>otetsudai</h1>
-      </Link>
-      <div className={styles.navigation_link}>
-        <Link href="/createRecruitment" className={styles.recruitment_link}>
-          募集する
-        </Link>
-        <Link href="/help " className={styles.help_link}>
-          help
-        </Link>
-        {user ? (
-          <Link
-            href={`/userProfile/${username}`}
-            className={styles.account_link}
-          >
-            profile
-          </Link>
-        ) : (
-          <Link href="/login" className={styles.login_link}>
-            ログイン
-          </Link>
-        )}
-      </div>
-    </div>
-  );
+  return <NavigationUI user={user} username={username} />;
 };
 
 export default Navigation;
