68ddd90b28e86eada1292fc722732193ac24e48b
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-06-19 15:02:38 +0900

    [add]EditButtonのテストの追加

--- test/component/recruitment/button/edit/EditButton.test.tsx
+++ test/component/recruitment/button/edit/EditButton.test.tsx
@@ -1,7 +1,11 @@
 import { describe, expect, test, vi } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import EditButton from "@/components/recruitment/recruitmentBtn/edit/EditButton";
 import { RecruitmentWithProfile } from "@/types/supabase/types";
+import { updateRecruitment } from "@/lib/supabase_function/recruitment";
+vi.mock("@/lib/supabase_function/recruitment", () => ({
+  updateRecruitment: vi.fn(),
+}));
 
 const mockRecruitmentData: RecruitmentWithProfile = {
   id: 1,
@@ -40,5 +44,66 @@ describe("EditButton Component", () => {
 
     editButton.click();
     expect(screen.getByText("内容を編集する")).toBeInTheDocument();
+  });
+  test("モーダルを閉じると非表示になる", () => {
+    render(
+      <EditButton
+        recruitmentData={mockRecruitmentData}
+        onUpdate={mockOnUpdate}
+      />,
+    );
+    fireEvent.click(screen.getByRole("button", { name: "内容を編集する" }));
+
+    fireEvent.click(screen.getByRole("button", { name: "×" }));
+
+    expect(screen.queryByText("保存")).not.toBeInTheDocument();
+  });
+
+  test("保存処理で onUpdate が呼ばれる", async () => {
+    render(
+      <EditButton
+        recruitmentData={mockRecruitmentData}
+        onUpdate={mockOnUpdate}
+      />,
+    );
+
+    fireEvent.click(screen.getByRole("button", { name: "内容を編集する" }));
+
+    fireEvent.change(screen.getByLabelText("タイトル"), {
+      target: { value: "新しいタイトル" },
+    });
+    fireEvent.change(screen.getByLabelText("内容"), {
+      target: { value: "新しい説明" },
+    });
+
+    fireEvent.click(screen.getByRole("button", { name: "保存" }));
+
+    await waitFor(() => {
+      expect(mockOnUpdate).toHaveBeenCalledWith({
+        title: "新しいタイトル",
+        explanation: "新しい説明",
+      });
+    });
+  });
+
+  test("保存時に updateRecruitment が呼ばれる", async () => {
+    render(
+      <EditButton
+        recruitmentData={mockRecruitmentData}
+        onUpdate={mockOnUpdate}
+      />,
+    );
+
+    fireEvent.click(screen.getByRole("button", { name: "内容を編集する" }));
+
+    fireEvent.click(screen.getByRole("button", { name: "保存" }));
+
+    await waitFor(() => {
+      expect(updateRecruitment).toHaveBeenCalledWith(
+        mockRecruitmentData.id,
+        "新しいタイトル",
+        "新しい説明",
+      );
+    });
   });
 });
