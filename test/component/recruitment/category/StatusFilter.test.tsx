d7a974ac0afa67c4b54d240fb01711a737d3321d
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-06-19 15:03:08 +0900

    [clean]StatusFilterのファイル名修正

--- test/component/recruitment/category/StatusFilter.test.tsx
+++ test/component/recruitment/category/StatusFilter.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import StatusFilter from "@/components/recruitment/category/filter";
+import StatusFilter from "@/components/recruitment/category/StatusFilter";
 import { STATUS_VALUES } from "@/utils/enum/enum";
 
 describe("StatusFilter", () => {
