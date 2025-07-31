bf781fa6dd38b7a73c6c4edbbf60835792a60fed
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-07-31 15:10:22 +0900

    [add]navigaitonに検索フォームの追加

---
+++ components/navigation/search/SearchForm.tsx
@@ -0,0 +1,32 @@
+"use client";
+import styles from "./search.module.css";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const SearchForm = () => {
+  const router = useRouter();
+  const [keyword, setKeyword] = useState("");
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (keyword.trim() === "") {
+      return;
+    }
+    router.push(`/search/${keyword}`);
+  };
+  return (
+    <form className={styles.search_form} onSubmit={handleSubmit}>
+      <input
+        className={styles.search_input}
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        type="text"
+        placeholder="Search..."
+      />
+      <button className={styles.search_button} type="submit">
+        検索
+      </button>
+    </form>
+  );
+};
+
+export default SearchForm;
