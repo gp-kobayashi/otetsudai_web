e697f04410492e3411ae3c608c09a32a7ef52852
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-08-18 16:41:53 +0900

    [add]message用の型定義の追加

--- types/supabase/types.ts
+++ types/supabase/types.ts
@@ -6,6 +6,8 @@ export type Comment = Database["public"]["Tables"]["comments"]["Row"];
 
 export type Profile = Database["public"]["Tables"]["profiles"]["Row"];
 
+export type Message = Database["public"]["Tables"]["messages"]["Row"];
+
 export type statusEnumType = Database["public"]["Enums"]["status_enum"];
 
 export type RecruitmentWithProfile =
@@ -17,6 +19,15 @@ export type CommentWithProfile =
   Comment & {
     avatar_url: string;
   } & { username: string | null };
+
+export type MessageWithProfile =
+  Message & {
+    sender_avatar_url: string;
+    receiver_avatar_url: string;
+  } & {
+    sender_username: string | null;
+    receiver_username: string | null;
+  };
   
 export type SupabaseResponse<T> = {
   data: T | null;
