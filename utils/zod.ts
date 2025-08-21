9ef74f502d7632a60cb2f7e01bfc14992c4964e2
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-08-21 15:16:17 +0900

    [add]zodにてバリデーションの適応

--- utils/zod.ts
+++ utils/zod.ts
@@ -32,4 +32,13 @@ export const searchSchema = z.object({
     .regex(/^[\p{Script=Latin}\p{Nd}\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}\s\u3000、。・ー〜！？「」『』（）\[\]【】]+$/u, "特殊文字は使用できません"),
 });
 
-   
+export const messageSchema = z.object({
+  title: z
+    .string()
+    .min(1, "タイトルは1文字以上で入力してください")
+    .max(100, "タイトルは100文字以内で入力してください"),
+  text: z
+    .string()
+    .min(1, "メッセージ本文は1文字以上で入力してください")
+    .max(500, "メッセージ本文は500文字以内で入力してください"),
+});
